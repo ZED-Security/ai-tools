7adf1de0d525859f541dd3ff540a0484ec5446ed
ZED Security <gamaymaster2022@gmail.com>
2025-09-22 20:10:54 +0300

    End update...

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from "react-router-dom";
 
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 )
